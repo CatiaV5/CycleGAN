1e6bf91c501466e8393373119034b582a7fd9f0a
CatiaV5 <pizza.spinate@gmail.com>
2024-01-29 09:50:46 +0100

    final train and utils files

--- utils.py
+++ utils.py
@@ -41,3 +41,98 @@ class ImageDataset(Dataset):
         """
 
         def __getitem__(self,index):
+            image_A = Image.open(self.files_A[index % len(self.files_A)])
+            # a % b => a is divided by b, and the remainder of that division is returned
+
+            if self.unaligned:
+                image_B = Image.open(self.files_B[random.randint(0, len(self.files_B) - 1)])
+            else:
+                image_B = Image.open(self.files_B[index % len(self.files_B)])
+
+            # Convert grayscale images to rgb
+            if image_A.mode != "RGB":
+                image_A = convert_to_rgb(image_A)
+            if image_B.mode != "RGB":
+                image_B = convert_to_rgb(image_B)
+
+            item_A = self.transform(image_A)
+            item_B = self.transform(image_B)
+
+            # Finally return a dict
+            return {"A": item_A, "B": item_B}
+
+        def __len__(self):
+            return max(len(self.files_A), len(self.files_B))
+
+###############################################
+# Replay Buffer
+###############################################
+"""
+As per the paper - To reduce model oscillation, we update the discriminator using a history of generated images
+rather than the ones produced bz the latest generators. We keep an image buffer that stores the 50 previously created 
+images.
+And here the followed - https://arvix.org/pdf/1612.07828.pdf
+
+This is another strategy used to stabilise the CycleGAN Training
+Replay buffer is used to train the discriminator. Generated images are added to the replay buffer and sampled from it.
+The replay buffer returns the newly added image with a probabilty of 0.5
+Otherwise, it sends an older generated image and replaces the older image with the newly generated image.
+This is done to reduce model oscillation.
+"""
+
+class ReplayBuffer:
+    def __init__(self, max_size=50):
+        assert max_size > 0, "Empty buffer"
+        self.max_size = max_size
+        self.data = []
+
+    def push_and_pop(self, data):
+        to_return = []
+        for element in data.data:
+            element = torch.unsqueeze(element, 0)
+            if len(self.data) < self.max_size:
+                self.data.append(element)
+                to_return.append(element)
+            else:
+                # returns newly added image with a probability of 0.5
+                if random.uniform(0,1) > 0.5:
+                    i = random.randint(0, self.max_size - 1)
+                    to_return.append(self.data[i].clone())
+                    self.data[i] = element
+                else:
+                    # Otherwise, it sends an older generated image and replaces the older image with the newly
+                    # generated image.
+                    to_return.append(element)
+        return Variable(torch.cat(to_return))
+
+###############################################
+# Learning Rate scheduling with 'lr_lambda'
+###############################################
+class LambdaLR:
+    def __init__(self, n_epochs, offset, decay_start_epoch):
+        assert (
+            n_epochs - decay_start_epoch
+        ) > 0, "Decay must start before the training sesssion ends!"
+        self.n_epochs = n_epochs
+        self.offset = offset
+        self.decay_start_epoch = decay_start_epoch
+
+    def step(self, epoch):
+        # Below line checks whether the current epoch has exceeded the decaz epoch(which is 100)
+        # e.g. if current epoch is 80 then max (0, 80 - 100) will be 0
+        # i.e. then entire numerator will be 0 - so 1 - 0 is 1
+        # i.e. the orginial LR remains as it is.
+        return 1.0 - max(0, epoch + self.offset - self.decay_start_epoch) / (self.n_epochs - self.decay_start_epoch)
+
+###############################################
+# Initialise convolutional layer weights to N(0, 0.02)
+###############################################
+def initialize_conv_weights_normal(m):
+    classname = m.__class__.__name__
+    if classname.find("Conv") != -1:
+        torch.nn.init.normal(m.weight.data, 0.0, 0.02)
+        if hasattr(m, "bias") and m.bias is not None:
+            torch.nn.init.constant_(m.bias.data, 0.0)
+    elif classname.find("BatchNorm2d") != -1:
+        torch.nn.init.normal_(m.weight.data, 1.0, 0.02)
+        torch.nn.init.constant_(m.bias.data, 0.0)
